2530ba2bf88280c19e0f3c81dce1c4af4f49b7b6
kajikentaro <k.kentarou0412@gmail.com>
2022-07-19 13:29:42 +0000

    Update: ejsをhtmlに変換するwebpack.config.jsを作成

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const glob = require("glob");
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const TRASH_FILE_NAME = "TO_BE_TRASH.trash"
 
 const ejs_list = glob.sync("./src/**/[!_]*.ejs");
 const ejs_entry = ejs_list.map((v) => {
-  const without_extension = v.match("(.+/.+?).[a-z]+([?#;].*)?$")[1];
-  return [without_extension + ".html", v];
+  return [TRASH_FILE_NAME, v];
 });
 const entry = Object.fromEntries(ejs_entry);
 
@@ -29,10 +30,14 @@ module.exports = {
     ],
   },
   plugins: [
+    new RemoveEmptyScriptsPlugin({
+      extensions: /\.(css|scss|sass|less|styl|ejs|html)([?].*)?$/,
+      remove: /\.(js|mjs|trash)$/
+    }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'src/index.ejs',
-      minify: false,
+      minify: true
     })
   ]
 };
