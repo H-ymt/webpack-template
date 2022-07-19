4bdde78ed22bc8a94797fe547f9e3f3b0c62895c
kajikentaro <k.kentarou0412@gmail.com>
2022-07-19 17:17:32 +0000

    Add: scssを記述できるように

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,40 @@
 const glob = require("glob");
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
-const TRASH_FILE_NAME = "TO_BE_TRASH.trash"
+
+const removeExtension = (path) => {
+  return path.match(/(.+\/.+?).[a-z]+([?#;].*)?$/)[1];
+}
+const removeSrc = (path) => {
+  return path.replace(/src\//, "")
+}
+// srcディレクトリと拡張子を消す
+// ex: ./src/index.html -> ./index
+const arrangePath = (fullPath) => {
+  return removeSrc(removeExtension(fullPath));
+}
 
 const ejs_list = glob.sync("./src/**/[!_]*.ejs");
-const ejs_entry = ejs_list.map((v) => {
-  return [TRASH_FILE_NAME, v];
-});
-const entry = Object.fromEntries(ejs_entry);
+const entry_list = ejs_list.map((v) => [removeSrc(v), v])
+const entry = Object.fromEntries(entry_list);
+
+const html_list = ejs_list.map((v) => {
+  return new HtmlWebpackPlugin({
+    filename: arrangePath(v) + ".html",
+    template: v,
+    minify: true
+  })
+})
+
+console.log(entry);
 
 module.exports = {
   entry: entry,
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name]",
+    filename: "[name].js",
   },
   module: {
     rules: [
@@ -25,17 +45,20 @@ module.exports = {
           "ejs-html-loader"
         ],
       },
+      {
+        test: /\.(scss|sass|css)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
     new RemoveEmptyScriptsPlugin({
       extensions: /\.(css|scss|sass|less|styl|ejs|html)([?].*)?$/,
-      remove: /\.(js|mjs|trash)$/
+      remove: /\.(js|mjs)$/
     }),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.ejs',
-      minify: true
-    })
+    ...html_list,
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
   ],
 };
